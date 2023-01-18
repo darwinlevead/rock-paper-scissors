b76fab39ee4a24ba58cdda1b9f46362c541cd275
Darwin Levead <56140521+darwinlevead@users.noreply.github.com>
2023-01-17 22:07:22 -0600

    create game in page

--- game.js
+++ game.js
@@ -1,35 +1,44 @@
-let player = prompt('Select: 1: 🪨, 2:📄 or 3: ✂️.');
-if(+player === 1){
-    player = '🪨';
-} else if(+player === 2){
-    player = '📄';
-} else if(+player === 3){
-    player = '✂️';
-} else{
-    player = 'Please select again: 🪨📄✂️'
-}
-alert(`You select: ${player}`);
-
-let computer = Math.round((Math.random()*2)+1);
-if(computer === 1){
-    computer = '🪨';
-} else if(computer === 2){
-    computer = '📄';
-} else if(computer === 3){
-    computer = '✂️';
-} else{
-    computer = 'Please select again: 🪨📄✂️'
-}
-alert(`Computer select: ${computer}`);
-
-if(computer === player){
-    alert('Tie.')
-} else if(computer === '🪨' && player === '📄'){
-    alert('You gain.')
-} else if(computer === '📄' && player === '✂️'){
-    alert('You gain.')
-} else if(computer === '✂️' && player === '🪨'){
-    alert('You gain.')
-}else{
-    alert('You lose.')
-}
+function myFunction() {
+    let player = document.getElementById("number").value;
+    if(+player < 4){
+        if(+player === 1){
+            player = '🪨';
+        } else if(+player === 2){
+            player = '📄';
+        } else if(+player === 3){
+            player = '✂️';
+        }
+        document.getElementById('player').innerHTML = player
+        
+        let computer = Math.round((Math.random()*2)+1);
+        if(computer === 1){
+            computer = '🪨';
+        } else if(computer === 2){
+            computer = '📄';
+        } else if(computer === 3){
+            computer = '✂️';
+        } else{
+            computer = 'Please select again: 🪨📄✂️'
+        }
+        document.getElementById('computer').innerHTML = computer
+        
+        if(computer === player){
+            document.getElementById('result').innerHTML = 'tie'
+        } else if(computer === '🪨' && player === '📄'){
+            document.getElementById('result').innerHTML = 'gain'
+        } else if(computer === '📄' && player === '✂️'){
+            document.getElementById('result').innerHTML = 'gain'
+        } else if(computer === '✂️' && player === '🪨'){
+            document.getElementById('result').innerHTML = 'gain'
+        }else{
+            document.getElementById('result').innerHTML = 'lose'
+        }
+    } else{
+        player = 'Please select again: 🪨📄✂️'
+        document.getElementById('player').innerHTML = player
+        document.getElementById('computer').innerHTML = ' '
+        document.getElementById('result').innerHTML = ' '
+    }
+    
+  }
+//let player = prompt('Select: 1: 🪨, 2:📄 or 3: ✂️.');
